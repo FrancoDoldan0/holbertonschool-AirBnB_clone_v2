8cde6525091af3469592415adec65aedb269c834
FrancoDoldan0 <francodoldan23@gmail.com>
2024-03-25 15:27:43 -0700

    add task 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
+storage_type = os.getenv('HBNB_TYPE_STORAGE')
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if storage_type == 'db':
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
