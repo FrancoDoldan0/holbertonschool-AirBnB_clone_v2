5201a46e52ee6d940f3171c331112a0a2aaefcf8
jorge871 <jg303114@gmail.com>
2024-03-25 22:47:56 -0300

    fixed pycodestyle

--- models/user.py
+++ models/user.py
@@ -12,7 +12,7 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True) 
+    first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
 
     if storage_type == 'db':
