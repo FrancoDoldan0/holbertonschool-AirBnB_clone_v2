779f3ffc0b694de167d9c96c0f7fceb33115e2ff
jorge871 <jg303114@gmail.com>
2024-03-28 14:29:54 -0300

    updated classes

--- models/user.py
+++ models/user.py
@@ -4,19 +4,18 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import os
-
-
+storage_type = os.getenv('HBNB_TYPE_STORAGE')
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    storage_type = os.getenv('HBNB_TYPE_STORAGE')
+
     if storage_type == 'db':
-        places = relationship('Place', cascade='all, delete-orphan', backref='user')
+        places = relationship('Place', cascade='all, delete-orphan',
+                              backref='user')
 
