076575622964f1e46894dc864b6a914072575a92
jorge871 <jg303114@gmail.com>
2024-03-29 03:08:54 +0000

    update user clas

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import os
-storage_type = os.getenv('HBNB_TYPE_STORAGE')No se ha podido resolver la importación "sqlalchemy"
+storage_type = os.getenv('HBNB_TYPE_STORAGE')
 
 
 class User(BaseModel, Base):
